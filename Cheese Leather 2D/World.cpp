9aa578ad3d664554ef7c4ff8ed28392c8d3a40b1
ostkaka <john.ostkaka@gmail.com>
2014-12-20 16:43:05 +0100

    git är mytoman!

--- Cheese Leather 2D/World.cpp
+++ Cheese Leather 2D/World.cpp
@@ -14,7 +14,6 @@ World::~World()
 
 void World::Render(Game *game)
 {
-<<<<<<< HEAD
 	for (int x = 0; x < 4; ++x) {
 		for (int y = 0; y < 4; ++y) {
 			auto it = m_chunks.find(i32vec2(x, y));
@@ -30,20 +29,13 @@ void World::Render(Game *game)
 			chunk->Render(game);
 		}
 	}
-=======
-
->>>>>>> 43788811d3ad1f01578a98bc99667ac19445ded8
 }
 
 /******************************************
 *              Private                    *
-<<<<<<< HEAD
 *******************************************/
 
 Chunk *World::getChunk(long x, long y)
 {
 	return nullptr;
 }
-=======
-*******************************************/
->>>>>>> 43788811d3ad1f01578a98bc99667ac19445ded8
