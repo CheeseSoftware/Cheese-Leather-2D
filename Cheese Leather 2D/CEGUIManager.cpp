086d097ce684f27f982c69ef1618b548d917e6d6
ostkaka <john.ostkaka@gmail.com>
2015-01-01 20:08:35 +0100

    CEGUIManager is working!

--- Cheese Leather 2D/CEGUIManager.cpp
+++ Cheese Leather 2D/CEGUIManager.cpp
@@ -41,12 +41,12 @@ void CEGUIManager::init(Window *window) {
 		// set resource group directories.
 		{
 			CEGUI::DefaultResourceProvider* rp = resourceProvider;
-			rp->setResourceGroupDirectory("schemes", "../datafiles/schemes/");
-			rp->setResourceGroupDirectory("imagesets", "../datafiles/imagesets/");
-			rp->setResourceGroupDirectory("fonts", "../datafiles/fonts/");
-			rp->setResourceGroupDirectory("layouts", "../datafiles/layouts/");
-			rp->setResourceGroupDirectory("looknfeels", "../datafiles/looknfeel/");
-			rp->setResourceGroupDirectory("lua_scripts", "../datafiles/lua_scripts/");
+			rp->setResourceGroupDirectory("schemes", "datafiles/schemes/");
+			rp->setResourceGroupDirectory("imagesets", "datafiles/imagesets/");
+			rp->setResourceGroupDirectory("fonts", "datafiles/fonts/");
+			rp->setResourceGroupDirectory("layouts", "datafiles/layouts/");
+			rp->setResourceGroupDirectory("looknfeels", "datafiles/looknfeel/");
+			rp->setResourceGroupDirectory("lua_scripts", "datafiles/lua_scripts/");
 		}
 
 		// set default resource groups.
@@ -61,29 +61,22 @@ void CEGUIManager::init(Window *window) {
 			CEGUI::XMLParser* parser = CEGUI::System::getSingleton().getXMLParser();
 			if (parser->isPropertyPresent("SchemaDefaultResourceGroup"))
 				parser->setProperty("SchemaDefaultResourceGroup", "schemas");
+
+
+			CEGUI::SchemeManager::getSingleton().createFromFile("TaharezLook.scheme");
 		}
 	}
 
-	// Create a root window
+	// Create windows
 	{
 		CEGUI::WindowManager& wmgr = CEGUI::WindowManager::getSingleton();
 
-		CEGUI::Window* myRoot = wmgr.createWindow("DefaultWindow", "root");
+
+		CEGUI::Window* myRoot = CEGUI::WindowManager::getSingleton().loadLayoutFromFile("test.layout");
 		CEGUI::System::getSingleton().getDefaultGUIContext().setRootWindow(myRoot);
+		CEGUI::FrameWindow* fWnd = static_cast<CEGUI::FrameWindow*>(
+			wmgr.createWindow("TaharezLook/FrameWindow", "testWindow"));
 
-		// TODO: Fix this, it won't work. Loading the theme/scheme "TaharezLook" might be required. >.<
-		//CEGUI::FrameWindow* fWnd = static_cast<CEGUI::FrameWindow*>(
-		//wmgr.createWindow("FrameWindow", "testWindow"));
-
-		//myRoot->addChild(fWnd);
-
-
-		//// position a quarter of the way in from the top-left of parent.
-		//fWnd->setPosition(CEGUI::UVector2(CEGUI::UDim(0.25f, 0.0f), CEGUI::UDim(0.25f, 0.0f)));
-		//// set size to be half the size of the parent
-		//fWnd->setSize(CEGUI::USize(CEGUI::UDim(0.5f, 0.0f), CEGUI::UDim(0.5f, 0.0f)));
-
-		//fWnd->setText("Hello World!");
 	}
 
 }
